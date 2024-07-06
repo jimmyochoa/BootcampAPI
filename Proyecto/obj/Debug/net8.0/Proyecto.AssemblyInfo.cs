b55b677fcf3d17731ca8105642891d8f4cddfcdd
jimmyochoa <jimmyochoaholguin@gmail.com>
2024-07-05 19:12:30 -0500

    first commit

--- Proyecto/obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c774b34d5827064095faaa3cb32164dbfd2d38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
