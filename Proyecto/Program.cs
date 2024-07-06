da9ddc77fc3b12d1699d9309178f2274aed0ac27
jimmyochoa <jimmyochoaholguin@gmail.com>
2024-07-05 19:48:04 -0500

    Correcion y uso de VentaService

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IProducto, ProductoService>();
 builder.Services.AddScoped<ICatalogo, CatalogoService>();// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped<ICliente, ClienteService>();
+builder.Services.AddScoped<IVentas, VentasServices>(); // Example registration
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
